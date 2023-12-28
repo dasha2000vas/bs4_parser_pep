90b859801addc88aa4f5d68238b3b9649e5ace7c
Дарья Василевская <vasilevsckaya.dascha@yandex.ru>
2023-12-28 18:11:39 +0300

    Add pep mode

---
+++ src/constants.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+MAIN_DOC_URL = 'https://docs.python.org/3/'
+MAIN_PEP_URL = 'https://peps.python.org/'
+BASE_DIR = Path(__file__).parent
+EXPECTED_STATUS = {
+    'A': ('Active', 'Accepted'),
+    'D': ('Deferred',),
+    'F': ('Final',),
+    'P': ('Provisional',),
+    'R': ('Rejected',),
+    'S': ('Superseded',),
+    'W': ('Withdrawn',),
+    '': ('Draft', 'Active'),
+}
